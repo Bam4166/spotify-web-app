a17dc19d98ee79af572878369d539a5a93455bad
Bam4166 <andrefmartins92@hotmail.com>
2022-01-26 18:52:16 +0000

    Defined initial app structure and components

---
+++ src/Components/App/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import {Playlist} from '../Playlist/Playlist';
+import {SearchBar} from '../SearchBar/SearchBar';
+import {SearchResults} from '../SearchResults/SearchResults';
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+             <SearchResults />
+             <Playlist />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/Components/Track/Track.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Track.css';
+
+export class Track extends React.Component {
+
+  renderAction() {
+    this.props.isRemoval ? 
+      <button className="Track-action">-</button> : 
+      <button className="Track-action">+</button>
+  }
+
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>
+            {
+              // Track name
+            }
+          </h3>
+          <p>
+            {
+              // Track Artist | Track Album
+            }
+          </p>
+        </div>
+        <button className="Track-action">
+          {this.renderAction}
+        </button>
+      </div>
+    )
+  }
+
+}
